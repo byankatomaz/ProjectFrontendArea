1027cf1b2a1ac91aa1282d5c27ee916e4c8b71a1
byankatomaz <byankatomaz@gmail.com>
2024-05-02 09:43:20 -0300

    env: vite config

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(),],
+  resolve: {
+    alias: {
+      src: "/src",
+      pages: "/src/pages",
+      components: "/src/components",
+      routes: "/src/routes",
+      services: "/src/services",
+      validations: "/src/validations",
+      providers: "/src/providers",
+      context: "/src/context",
+    },
+  },
+})
