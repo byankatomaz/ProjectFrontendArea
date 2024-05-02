be9b6c1eadf97a3097903f93b5e3f7fac5d42ab4
byankatomaz <byankatomaz@gmail.com>
2024-05-02 09:40:04 -0300

    feat: component table

---
+++ src/components/Open/Open.tsx
@@ -0,0 +1,112 @@
+import { Card, Typography } from "@material-tailwind/react";
+
+const TABLE_HEAD = ["Title", "Year", "Status", ""];
+
+const TABLE_ROWS = [
+    {
+        title: "Name Example",
+        year: "2024",
+        status: "Open",
+    },
+    {
+        title: "Name Example",
+        year: "2024",
+        status: "Open",
+    },
+    {
+        title: "Name Example",
+        year: "2024",
+        status: "Open",
+    },
+    {
+        title: "Name Example",
+        year: "2024",
+        status: "Open",
+    },
+    {
+        title: "Name Example",
+        year: "2024",
+        status: "Open",
+    },
+];
+
+
+export function Open() {
+    return (
+        <Card className="h-full w-full overflow-scroll mt-3">
+            <div className="flex justify-center py-5">
+                <h1 className="text-black text-lg font-semibold">Breakdown Abertas</h1>
+            </div>
+
+            <table className="w-full min-w-max table-auto text-left ">
+                <thead>
+                    <tr>
+                        {TABLE_HEAD.map((head) => (
+                            <th
+                                key={head}
+                                className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
+                            >
+                                <Typography
+                                    variant="small"
+                                    color="blue-gray"
+                                    className="font-normal leading-none opacity-70"
+                                >
+                                    {head}
+                                </Typography>
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {TABLE_ROWS.map(({ title, year, status }, index) => {
+                        const isLast = index === TABLE_ROWS.length - 1;
+                        const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
+
+                        return (
+                            <tr key={title}>
+                                <td className={classes}>
+                                    <Typography
+                                        variant="small"
+                                        color="blue-gray"
+                                        className="font-normal"
+                                    >
+                                        {title}
+                                    </Typography>
+                                </td>
+                                <td className={classes}>
+                                    <Typography
+                                        variant="small"
+                                        color="blue-gray"
+                                        className="font-normal"
+                                    >
+                                        {year}
+                                    </Typography>
+                                </td>
+                                <td className={classes}>
+                                    <Typography
+                                        variant="small"
+                                        color="blue-gray"
+                                        className="font-normal"
+                                    >
+                                        {status}
+                                    </Typography>
+                                </td>
+                                <td className={classes}>
+                                    <Typography
+                                        as="a"
+                                        href="#"
+                                        variant="small"
+                                        color="blue-gray"
+                                        className="font-medium"
+                                    >
+                                        Edit
+                                    </Typography>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </Card>
+    );
+}
